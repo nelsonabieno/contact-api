568d072e1a0c92e4ce5b65f64d9f61087c919b81
nelsonabieno <=>
2018-10-16 22:25:51 +0100

    jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,37 @@
 class ApplicationController < ActionController::API
-  # protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller
 
-  protected
+  before_action :authenticate
+  skip_before_action :authenticate, only: [:create]
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-        :password, :phoneno, :role_id, :status, :email, :first_name, :last_name, :username, :date_of_birth ) }
-
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(
-        :email, :password, :remember_me) }
-
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-        :password, :phoneno, :role_id, :status,:email, :first_name, :last_name, :username, :date_of_birth ) }
-
+  def logged_in?
+    !! current_user
   end
 
+  def current_user
+    if auth_present?
+      user = User.find(auth['user'])
+      if user && user.status
+        @current_user ||= user
+      end
+    end
+  end
+
+  def authenticate
+    render json: { error: 'unauthorized' }, status: 401 unless logged_in?
+  end
+
+  private
+
+  def token
+    request.env['HTTP_HTTP_AUTHORIZATION'].scan(/Bearer(.*)$/).flatten.last
+  end
+
+  def auth
+    Auth.decode(token)
+  end
+
+  def auth_present?
+    !!request.env.fetch('HTTP_HTTP_AUTHORIZATION',
+    '').scan(/Bearer/).flatten.first
+  end
 end
