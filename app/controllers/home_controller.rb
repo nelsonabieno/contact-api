3f6f6b37fed04fb47fed559a76ffe7d8c0e712f8
nelsonabieno <=>
2018-10-18 11:18:07 +0100

    skip authentication for home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate, only: [:index]
   def index
     render json: { message: 'welcome to qontactsky contact mgt app'}, status: :ok
   end
