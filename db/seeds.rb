709cb37b2946dc1a239bc72e34a0e098e8b23e4d
nelsonabieno <=>
2018-10-18 12:54:33 +0100

    refactor seeder to use environment config

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,26 +10,14 @@
 
 # 3.times { |i| Role.create({  id: i+1, name: roles[i] }) }
 
-#
-# password = ENV['SUPER_PASSWORD']
-# phoneno = ENV['SUPER_PHONE']
-# email = ENV['SUPER_EMAIL'],
-# first_name = ENV['SUPER_FIRSTNAME']
-# last_name =  ENV['SUPER_LASTNAME']
-
-password = 'admin_admin'
-phoneno = '08163816325'
-email = 'contactadmin@gmail.com'
-first_name = 'nelson'
-last_name =  'rotimi'
-
 user = User.new({
-                    password: password,
-                    phoneno: phoneno,
+                    password: ENV['SUPER_PASSWORD'],
+                    phoneno: ENV['SUPER_PHONE'],
                     role_id: 1,
                     status: false,
-                    email: email,
-                    first_name: first_name,
-                    last_name: last_name
+                    email: ENV['SUPER_EMAIL'],
+                    first_name: ENV['SUPER_FIRSTNAME'],
+                    last_name: ENV['SUPER_LASTNAME']
                 })
+
 user.save! if user.valid?
